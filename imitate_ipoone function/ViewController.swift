e7352606e25cfe9a4452893a1f930473fba2d5d3
brianke <oooreo612@gmail.com>
2022-03-10 13:34:04 +0800

    Initial Commit

---
+++ imitate_ipoone function/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imitate_ipoone function
+//
+//  Created by iroda on 2022/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
